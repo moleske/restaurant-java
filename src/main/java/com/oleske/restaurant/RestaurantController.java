3d66e1675f720d9a5e2125f9c79cebc779d241d0
David McClure <dmcclure@pivotal.io>
2016-09-30 17:32:48 -0700

    Remove autowired cause we don't need it?
    - one line that controller
    
    Signed-off-by: Michael Oleske <moleske@pivotal.io>

--- src/main/java/com/oleske/restaurant/RestaurantController.java
+++ src/main/java/com/oleske/restaurant/RestaurantController.java
@@ -1,6 +1,5 @@
 package com.oleske.restaurant;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,15 +12,13 @@ public class RestaurantController {
 
     private RestaurantRepository restaurantRepository;
 
-    @Autowired
     public RestaurantController(RestaurantRepository restaurantRepository) {
         this.restaurantRepository = restaurantRepository;
     }
 
     @RequestMapping(value = "/newRestaurant", method = RequestMethod.POST)
     public ResponseEntity<Restaurant> createRestaurant(@RequestBody Restaurant restaurant) {
-        Restaurant savedRestaurant= restaurantRepository.save(restaurant);
-        return new ResponseEntity(savedRestaurant, HttpStatus.CREATED);
+        return new ResponseEntity(restaurantRepository.save(restaurant), HttpStatus.CREATED);
     }
 
 
