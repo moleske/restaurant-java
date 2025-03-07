ea03ef8705defb8e9fde78eb63fde6ca989db4ef
M. Oleske <michael@oleske.engineer>
2025-03-06 22:10:40 -0800

    Replace deprecated mockbean with mockitobean

--- src/test/java/com/oleske/restaurant/RestaurantControllerTest.java
+++ src/test/java/com/oleske/restaurant/RestaurantControllerTest.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -24,7 +24,7 @@ public class RestaurantControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
+    @MockitoBean
     private RestaurantRepository mockRestaurantRepository;
 
     private ObjectMapper objectMapper;
